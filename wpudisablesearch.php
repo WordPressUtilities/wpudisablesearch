69d613bd739bf9e9c65cf65f54404f867b49b9fd
Kevin <kevin@rocher-dev.com>
2021-05-18 19:38:41 +0200

    v 0.3.0
    - Remove search from WP API.

--- wpudisablesearch.php
+++ wpudisablesearch.php
@@ -3,13 +3,17 @@
 Plugin Name: WPU disable search
 Plugin URI: https://github.com/WordPressUtilities/wpudisablesearch
 Description: Disable search
-Version: 0.2.2
+Version: 0.3.0
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
 License URI: http://opensource.org/licenses/MIT
 Thanks: http://www.geekpress.fr/?p=722
 */
+
+/* ----------------------------------------------------------
+  Prevent query
+---------------------------------------------------------- */
 
 function wpu_disable_search__in_query($query, $error = true) {
     if (!is_search()) {
@@ -31,4 +35,23 @@ function wpu_disable_search__init() {
     }
     add_action('parse_query', 'wpu_disable_search__in_query');
     add_filter('get_search_form', '__return_null');
+}
+
+/* ----------------------------------------------------------
+  Remove from WP API
+---------------------------------------------------------- */
+
+add_filter('rest_endpoints', 'wpu_disable_search_rest_endpoints');
+function wpu_disable_search_rest_endpoints($endpoints) {
+    $endpoints_keys = array(
+        '/wp/v2/search'
+    );
+
+    foreach ($endpoints_keys as $key) {
+        if (isset($endpoints[$key])) {
+            unset($endpoints[$key]);
+        }
+    }
+
+    return $endpoints;
 }
